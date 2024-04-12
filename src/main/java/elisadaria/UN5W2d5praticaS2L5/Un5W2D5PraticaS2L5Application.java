b1f726a0ade5f131e6d4c87c9ebc313519d038cb
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-12 12:33:25 +0200

    add-all-dependencies+connecting-db

---
+++ src/main/java/elisadaria/UN5W2d5praticaS2L5/Un5W2D5PraticaS2L5Application.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W2d5praticaS2L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Un5W2D5PraticaS2L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Un5W2D5PraticaS2L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/elisadaria/UN5W2d5praticaS2L5/Un5W2D5PraticaS2L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W2d5praticaS2L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Un5W2D5PraticaS2L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
